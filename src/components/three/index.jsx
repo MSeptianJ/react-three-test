163b989602562f32c38d82797c9ad2d00ed898ee
Muhamad Septian Jaelani <mseptianjaelani@gmail.com>
2023-01-24 22:15:14 +0700

    Mouse Control Ball

--- src/components/three/index.jsx
+++ src/components/three/index.jsx
@@ -1,16 +1,37 @@
-import { PerspectiveCamera } from '@react-three/drei';
+import { OrbitControls, PerspectiveCamera, useGLTF } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
+import { useEffect, useRef } from 'react';
+import angleToRadians from '../../utils/angle';
 
 export default function Three() {
+	const orbitControlRef = useRef(null);
+	useFrame((state) => {
+		if (!!orbitControlRef.current) {
+			const { x, y } = state.mouse;
+			orbitControlRef.current.setAzimuthalAngle(-x * angleToRadians(45));
+			orbitControlRef.current.setPolarAngle(
+				(y + 0.5) * angleToRadians(90 - 30)
+			);
+			orbitControlRef.current.update();
+		}
+	});
+
 	return (
 		<>
 			<PerspectiveCamera makeDefault position={[0, 1, 15]} />
+			<OrbitControls
+				autoRotate="true"
+				ref={orbitControlRef}
+				minPolarAngle={angleToRadians(50)}
+				maxPolarAngle={angleToRadians(80)}
+			/>
 			{/* Ball */}
-			<mesh position={[0, 1, 0]}>
+			<mesh position={[0, 2, 0]}>
 				<sphereGeometry args={[1, 32, 32]} />
 				<meshStandardMaterial color="#4287f5" />
 			</mesh>
 			{/* Floor */}
-			<mesh rotation={[30, 0, 0]}>
+			<mesh rotation={[-angleToRadians(90), 0, 0]}>
 				<planeGeometry args={[7, 7]} />
 				<meshStandardMaterial color="#b7bec9" />
 			</mesh>
@@ -19,3 +40,11 @@ export default function Three() {
 		</>
 	);
 }
+
+// function Ship(props) {
+// 	const { scene } = useGLTF('/Duck.gltf');
+
+// 	return (
+// 			<primitive object={scene} />
+// 	);
+// }
